4696a6652768657b6a705ca04eaf277b4eb8d60e
cristobal rios <cristobal.rios01@alumnos.ucn.cl>
2024-11-14 03:21:51 -0300

    fix axios

--- services/serviceAxiosApi.ts
+++ services/serviceAxiosApi.ts
@@ -5,7 +5,7 @@ import { API_BASE_URL } from '@env';
 console.log('API BASE URL:', API_BASE_URL);
 
 const serviceAxiosApi = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: `${API_BASE_URL}`,
   headers: {
     'Content-Type': 'application/json',
   },
